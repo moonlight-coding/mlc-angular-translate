3e8d41f6a410dc83d4f84c58fd041d3328bafe28
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2019-10-05 01:25:31 +0200

    Update 2019-10-04T23:25:29.466Z

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ app.run(function(mlcTranslate, $http) {
   mlcTranslate.setLocale("fr_FR");
 });
 
-app.controller('MyCtrl', function($scope, mlcTranslate, mlcTranslateToolbox) {
+app.controller('MyCtrl', function($scope, mlcTranslate, mlcTranslateToolbox, Samples) {
   this.params = {
     timer: 0
   };
@@ -30,5 +30,29 @@ app.controller('MyCtrl', function($scope, mlcTranslate, mlcTranslateToolbox) {
   
   this.mlcTranslate = mlcTranslate;
   this.toolboxGroups = mlcTranslateToolbox.groups;
-  
+  this.samples = Samples;
+});
+
+app.directive('codeSample', function($sce) {
+  return {
+    template: '<pre><code ng-transclude></code></pre>',
+    transclude: true,
+    scope: {}
+  };
+});
+
+function escapeHtml(unsafe) {
+  return unsafe
+   .replace(/&/g, "&amp;")
+   .replace(/</g, "&lt;")
+   .replace(/>/g, "&gt;")
+   .replace(/"/g, "&quot;")
+   .replace(/'/g, "&#039;");
+}
+
+app.value('Samples', {
+  '1': `<div mlc-translate="'Hello'"></div>`,
+  '2': `<div mlc-translate="'Hello'" data-group="'my-group'"></div>`,
+  '3': `<div mlc-translate="'Timer value is {{ params.timer }}'" data-group="'my-group'" data-params="{timer: $ctrl.params.timer}"></div>`,
+  '4': `<div mlc-translate="'Timer value is {{ _.timer }}'" data-group="'my-group'" data-params="$ctrl.params"></div>`
 });
