c1fdaff68a082b9007d138140c9d8f9af91ae6fc
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2019-11-06 16:44:02 +0100

    added MlcTranslateLocalConnection to consume translation from local variables

---
+++ src/mlc-translate/services/mlc-translate/02-local-connection.js
@@ -0,0 +1,26 @@
+/**
+ * If the translations are available in local, you can integrate them in the application
+ */
+class MlcTranslateLocalConnection extends MlcTranslateAbstractApiConnection
+{
+  constructor(translationsPerLocale) {
+    super();
+    
+    this.translationsPerLocale = translationsPerLocale;
+  }
+  
+  /**
+   * Get the required groups
+   */
+  getLocale(locale, groups = null, history = false) {
+    return this.translationsPerLocale[locale];
+  };
+  
+  createTranslation(locale, group, key, value) {
+    throw new Error("Cannot create translation with MlcTranslateLocalConnection");
+  }
+  
+  removeTranslation(id) {
+    throw new Error("Cannot remove translation with MlcTranslateLocalConnection");
+  }
+}
